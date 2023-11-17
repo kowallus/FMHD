ee1453354db80336c8dd05f0d41d86cf14dcffde
kowallus <kowallus@o2.pl>
2023-11-17 13:08:54 +0100

    switched to 64-bit hash, processing with blocks (+candidates trick)

--- src/main.cc
+++ src/main.cc
@@ -29,18 +29,20 @@ int main (int argc, char **argv) {
     std::string cmd = "dist";
     size_t kmerlen = 21;
     bool edgelist = false;
-    int num_of_threads = 1;
+    const int max_reading_threads = 4;
+    int num_of_threads;
+#pragma omp parallel
+#pragma omp single
+    num_of_threads = omp_get_num_threads();
     app.add_option("-c,--cmd", cmd, "mode in which program operates")->required();
     app.add_option("-l,--inputlist", files_list, "filenames list for comparison");
     app.add_option("-k,--kmerlen", kmerlen, "k-mer lenght. Default: 21");
     app.add_option("-o,--outfile", out_file, "output filename with sketches. Default: sketch.bin");
-    app.add_option("-t,--nthreads", num_of_threads, "number of threads used for comparison. Default: 1");
+    app.add_option("-t,--nthreads", num_of_threads, "number of threads used for comparison.");
     app.add_flag("-E,--edgelist", edgelist, "return output as a edge list with fields [seq1, seq2, dist], default: triangle matrix");
     CLI11_PARSE(app, argc, argv);
 
     omp_set_dynamic(false);
-    omp_set_num_threads(num_of_threads);
-
     std::chrono::steady_clock::time_point start_t = std::chrono::steady_clock::now();
 
 //    appout = &null_stream;
@@ -53,25 +55,36 @@ int main (int argc, char **argv) {
 
         while (std::getline(input_file, line)) {
             files_names.push_back(line);
-            genomes.push_back(read_fna(line));
         }
         const size_t len_sketches = files_names.size();
-
+        genomes.resize(len_sketches);
+        int reading_threads = num_of_threads > max_reading_threads ? max_reading_threads : num_of_threads;
+        omp_set_num_threads(reading_threads);
+        *devout << "number of threads: " << reading_threads << std::endl;
+        #pragma omp parallel for
+        for (int i = 0; i < len_sketches; ++i) {
+            genomes[i] = read_fna(files_names[i]);
+        }
         *devout << "reading in "
                 << time_millis(start_t) << " msec" << std::endl;
         start_t = std::chrono::steady_clock::now();
 
+        omp_set_num_threads(num_of_threads);
+        *devout << "number of threads: " << num_of_threads << std::endl;
         std::vector<std::array<uint64_t, M>> sketches(len_sketches);
 
         #pragma omp parallel for
         for (int i = 0; i < len_sketches; ++i) {
             sketches[i] = get_sketch_hash_template<MA_RUSH_PRIME1_HASH_SIMPLIFIED_ID>(genomes[i], kmerlen);
 //            sketches[i] = get_sketch(genomes[i], (uint8_t) kmerlen, MA_RUSH_PRIME1_HASH_SIMPLIFIED_ID); // slower
-//            sketches[i] = get_sketch_full_template<4, NONE_HASH_ID>(genomes[i]); // only for testing, slower than without templates
+//            sketches[i] = get_sketch_full_template<8, NONE_HASH_ID>(genomes[i]); // only for testing, slower than without templates
         }
 
         save_sketches(sketches, files_names, out_file);
     } else {
+        omp_set_num_threads(num_of_threads);
+        *devout << "number of threads: " << num_of_threads << std::endl;
+
         std::vector<std::array<uint64_t, M>> sketches(0);
         std::vector<std::string> files_names(0);
         if (read_sketches(sketches, files_names, files_list))
--- src/sketch.cc
+++ src/sketch.cc
@@ -9,7 +9,7 @@
 
 // based on http://www.amsoftware.narod.ru/algo2.html
 template <size_t K>
-inline std::uint32_t maRushPrime1HashSimplified(const char *str) {
+inline std::uint64_t maRushPrime1HashSimplified(const char *str) {
     std::uint64_t hash = K;
     std::uint32_t k;
     for (std::uint32_t j = 0; j < K/4; ) {
@@ -19,15 +19,11 @@ inline std::uint32_t maRushPrime1HashSimplified(const char *str) {
         hash *= 171717;
         str += 4;
     }
-    if (K % 4) {
-        memcpy(&k, str, K % 4);
-        hash ^= K/4;
-        hash *= 171717;
-    }
-    return (std::uint32_t)(hash);
+    return (std::uint64_t)(hash);
 }
+
 template <size_t K>
-inline std::uint32_t xxhash64(const char *str) {
+inline std::uint64_t xxhash64(const char *str) {
     return XXHash64::hash(str, K, SEED);
 }
 
@@ -35,27 +31,57 @@ template <size_t K, int HASH_ID>
 inline uint64_t hash(char* input);
 
 template <> inline uint64_t hash<20, MA_RUSH_PRIME1_HASH_SIMPLIFIED_ID>(char* input) { return maRushPrime1HashSimplified<20>(input); }
-template <> inline uint64_t hash<21, MA_RUSH_PRIME1_HASH_SIMPLIFIED_ID>(char* input) { return maRushPrime1HashSimplified<21>(input); }
+template <> inline uint64_t hash<21, MA_RUSH_PRIME1_HASH_SIMPLIFIED_ID>(char* input) { return maRushPrime1HashSimplified<20>(input); }
 template <> inline uint64_t hash<24, MA_RUSH_PRIME1_HASH_SIMPLIFIED_ID>(char* input) { return maRushPrime1HashSimplified<24>(input); }
 template <> inline uint64_t hash<20, XXHASH64_ID>(char* input) { return xxhash64<20>(input); }
 template <> inline uint64_t hash<21, XXHASH64_ID>(char* input) { return xxhash64<21>(input); }
 template <> inline uint64_t hash<24, XXHASH64_ID>(char* input) { return xxhash64<24>(input); }
+template <> inline uint64_t hash<8, NONE_HASH_ID>(char* input) { return *((uint64_t*) input); }
 
 template <size_t K, int HASH_ID>
 inline void processInput(std::string &input, std::array<uint64_t, M> &hashes);
 
 template <size_t K, int HASH_ID>
-inline void processInput(std::string &input, std::array<uint64_t, M> &hashes) {
+inline void processInputInBlocks(std::string &input, std::array<uint64_t, M> &hashes) {
     const size_t guard = input.size() - K + 1;
-    for (size_t i = 0; i < guard; ++i) {
+    int MULTI_K = 64;
+    const size_t multi_guard = (guard / MULTI_K) * MULTI_K;
+    uint64_t multiHashes[MULTI_K];
+    size_t i = 0;
+    for (; i < multi_guard; i += MULTI_K) {
+        bool candidates = false;
+        for (int j = 0; j < MULTI_K; j++) {
+            multiHashes[j] = hash<K, HASH_ID>( input.data() + i + j );
+//            if (~(multiHashes[j] & 0xFFC0000000000000))
+            if ((multiHashes[j] >> 54) == 0)
+                candidates = true;
+        }
+        if (candidates)
+            for (int j = 0; j < MULTI_K; j++) {
+                const uint64_t hashValue = multiHashes[j];
+                uint64_t where = hashValue & MASK;
+                hashes[where] = std::min(hashes[where], hashValue);
+            }
+    }
+    for (; i < guard; ++i) {
         const uint64_t hashValue = hash<K, HASH_ID>( input.data() + i);
         uint64_t where = hashValue & MASK;
         hashes[where] = std::min(hashes[where], hashValue);
     }
 }
 
+template <size_t K, int HASH_ID>
+inline void processInput(std::string &input, std::array<uint64_t, M> &hashes) {
+    const size_t guard = input.size() - K + 1;
+    for (size_t i = 0; i < guard; ++i) {
+        const uint64_t hashValue = hash<K, HASH_ID>(input.data() + i);
+        uint64_t where = hashValue & MASK;
+        hashes[where] = std::min(hashes[where], hashValue);
+    }
+}
+
 template<>
-inline void processInput<4, NONE_HASH_ID>(std::string &input, std::array<uint64_t, M> &hashes)
+inline void processInput<8, NONE_HASH_ID>(std::string &input, std::array<uint64_t, M> &hashes)
 {
     const size_t guard = input.size() - 4 + 1;
     for ( size_t i = 0; i<guard; ++i) {
@@ -64,23 +90,35 @@ inline void processInput<4, NONE_HASH_ID>(std::string &input, std::array<uint64_
         hashes[where] = std::min(hashes[where], hashValue);
     }
 }
+
+template<>
+inline void processInputInBlocks<8, NONE_HASH_ID>(std::string &input, std::array<uint64_t, M> &hashes)
+{
+    const size_t guard = input.size() - 4 + 1;
+    for ( size_t i = 0; i<guard; ++i) {
+        const uint64_t hashValue = *((uint64_t*) (input.data() + i));
+        uint64_t where = hashValue & MASK;
+        hashes[where] = std::min(hashes[where], hashValue);
+    }
+}
+
 template <size_t K, int HASH_ID>
 inline std::array<uint64_t, M> get_sketch_full_template( std::string input )
 {
     std::array<uint64_t, M> hashes{};
     hashes.fill(std::numeric_limits<uint64_t>::max());
-    processInput<K, HASH_ID>(input, hashes);
+    processInputInBlocks<K, HASH_ID>(input, hashes);
     reverseComplementInPlace(input);
-    processInput<K, HASH_ID>(input, hashes);
+    processInputInBlocks<K, HASH_ID>(input, hashes);
     return hashes;
 }
 
 template <int HASH_ID>
 inline std::array<uint64_t, M> get_sketch_hash_template( std::string input, uint8_t kmerlen ) {
     if (kmerlen == 20) return get_sketch_full_template<20, HASH_ID>(input);
-    if (kmerlen == 21) return get_sketch_full_template<21, HASH_ID>(input);
     if (kmerlen == 24) return get_sketch_full_template<24, HASH_ID>(input);
-    std::cerr << "unsupported kmer length: " << kmerlen << std::endl;
+    if (HASH_ID != MA_RUSH_PRIME1_HASH_SIMPLIFIED_ID && kmerlen == 21) return get_sketch_full_template<21, HASH_ID>(input);
+    std::cerr << "unsupported kmer length: " << (int) kmerlen << " for hashID " << HASH_ID << std::endl;
     exit(EXIT_FAILURE);
 }
 
@@ -90,7 +128,7 @@ std::array<uint64_t, M> get_sketch( std::string input, const uint8_t kmerlen, co
     if (hash_id ==  XXHASH64_ID)
         return get_sketch_hash_template<XXHASH64_ID>(input, kmerlen);
     if (hash_id == NONE_HASH_ID)
-        return get_sketch_full_template<4, NONE_HASH_ID>(input);
+        return get_sketch_full_template<8, NONE_HASH_ID>(input);
     std::cerr << "unsupported hash type id: " << hash_id << std::endl;
     exit(EXIT_FAILURE);
 }
@@ -98,7 +136,7 @@ std::array<uint64_t, M> get_sketch( std::string input, const uint8_t kmerlen, co
 template std::array<uint64_t, M> get_sketch_hash_template<MA_RUSH_PRIME1_HASH_SIMPLIFIED_ID>( std::string , uint8_t );
 template std::array<uint64_t, M> get_sketch_hash_template<XXHASH64_ID>( std::string , uint8_t );
 
-template std::array<uint64_t, M> get_sketch_full_template<4, NONE_HASH_ID>( std::string );
+template std::array<uint64_t, M> get_sketch_full_template<8, NONE_HASH_ID>( std::string );
 
 template std::array<uint64_t, M> get_sketch_full_template<20, MA_RUSH_PRIME1_HASH_SIMPLIFIED_ID>( std::string );
 template std::array<uint64_t, M> get_sketch_full_template<21, MA_RUSH_PRIME1_HASH_SIMPLIFIED_ID>( std::string );
